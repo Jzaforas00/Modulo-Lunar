a59d1393aa060e56de9e47570e02b496be41aefc
Javier <zaforasvivesjavier@gmail.com>
2026-05-08 10:28:07 +0200

    Solution validations problems and change html

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, NgIf],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -16,7 +17,45 @@ export class AppComponent {
     id: '',
     nombre: '',
     grupo: '',
+    dureza: 1,
+    grano: '',
+    clasificacion: '',
+    cristales: 0,
+    temperatura: 0,
+    estructura: '',
+    formaGranos: '',
     textura: ''
   };
 
+  resultado: boolean | null = null;
+
+  analizar() {
+
+    const r = this.roca;
+
+    let valido = false;
+
+    if (r.grupo === 'Ígneas' && r.grano === 'Muy grueso') {
+      valido = true;
+    }
+
+    if (
+      r.grupo === 'Metamórficas' &&
+      (r.grano === 'Medio' || r.grano === 'Fino') &&
+      r.textura === 'Vítrea'
+    ) {
+      valido = true;
+    }
+
+    if (
+      r.grupo === 'Sedimentarias' &&
+      r.textura === 'Fanerítica'
+    ) {
+      valido = true;
+    }
+
+    this.resultado = valido;
+  }
+  
 }
+
